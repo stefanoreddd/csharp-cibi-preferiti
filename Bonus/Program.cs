7a88df44c60bd357d4289db5d01507068bde50ce
stefanoreddd <stefanogg95@gmail.com>
2023-04-14 17:37:41 +0200

    Bonus completed.

---
+++ Bonus/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+BONUS
+- Chiedere all'utente quanti cibi preferiti ha, e farglieli inserite da input della Console nell'array.
+- Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. Potrebbe essere interessante creare una funzione chiamata indiciMedianaArray(int lunghezzaArray) che vi restituisce l'indice della posizione mediana (che restituisca gli indici delle eventuali posizioni mediani) e poi fare una funzione (StampaArrayMediana) che si occupa di fare tutto il lavoro con  la funzione precedente e richiedendo direttamente l'intero array. Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica. 
+*/
+
+
+using System;
+using System.Runtime.Serialization.Formatters;
+
+Console.WriteLine("Quanti cibi preferiti hai?");
+string numeroCibiUser = Console.ReadLine();
+int numeroCibi = int.Parse(numeroCibiUser);
+
+string[] cibiPreferiti = new string[numeroCibi];
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine("Inserisci il " + (i + 1) + "° cibo:");
+    string ciboPreferito = Console.ReadLine();
+    cibiPreferiti[i] = ciboPreferito;
+}
+
+classificaCibiPreferiti(cibiPreferiti);
+posizioneMediana(cibiPreferiti);
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+
+void classificaCibiPreferiti(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine((i + 1) + ") " + array[i]);
+        Console.WriteLine("");
+    }
+
+}
+
+
+
+void posizioneMediana(string[] array)
+{
+    Console.WriteLine("Cibo preferito: " + array[0]);
+    Console.WriteLine("Cibo sfavorito: " + array[array.Length - 1]);
+    Console.WriteLine("Posizione mediana: " + array[array.Length / 2]);
+}
